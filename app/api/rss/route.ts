74dc5780b42cdbe75f839069b77ee581cd64552d
Sjors van Holst <sjorsvanholst@gmail.com>
2024-06-30 17:23:26 +0200

    ♻️ Changed image size

--- app/api/rss/route.ts
+++ app/api/rss/route.ts
@@ -19,6 +19,9 @@ export async function GET() {
     };
   });
 
+  const coverImage = encodeURIComponent(home.image.src);
+  const coverImageResized = `${url}/_next/image?url=${coverImage}&w=1920&q=75`;
+
   const feed = new RSS({
     feed_url: `${url}/api/rss`,
     site_url: url,
@@ -55,7 +58,7 @@ export async function GET() {
       {
         "itunes:image": {
           _attr: {
-            href: `${url}${home.image.src}`,
+            href: coverImageResized,
           },
         },
       },
@@ -64,6 +67,9 @@ export async function GET() {
   });
 
   podcasts.map((podcast) => {
+    const episodeImage = encodeURIComponent(podcast.image.src);
+    const episodeImageResized = `${url}/_next/image?url=${episodeImage}&w=1920&q=75`;
+
     feed.item({
       url: `${url}/podcast/${podcast.slug}`,
       date: podcast.publication,
@@ -73,7 +79,6 @@ export async function GET() {
       guid: podcast.slug,
       custom_elements: [
         { link: `${url}/podcast/${podcast.slug}` },
-        { image: `${url}${podcast.image.src}` },
         { pubDate: podcast.publication },
         { "itunes:title": podcast.title },
         { "itunes:episode": podcast.episode },
@@ -83,7 +88,7 @@ export async function GET() {
         {
           "itunes:image": {
             _attr: {
-              href: `${url}${podcast.image.src}`,
+              href: episodeImageResized,
             },
           },
         },

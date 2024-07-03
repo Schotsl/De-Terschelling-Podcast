d0a67f10c080bf47210957b0c7759f09b9d5cb0a
Sjors van Holst <sjorsvanholst@gmail.com>
2024-07-03 20:42:03 +0200

    💩 Added rough podcasts layout

--- components/Podcasts/index.tsx
+++ components/Podcasts/index.tsx
@@ -8,19 +8,22 @@ export default function Podcasts({ podcasts }: { podcasts: Podcast[] }) {
   return (
     <ul className={styles.podcasts}>
       {podcasts.map((podcast) => (
-        <li className={styles.podcast} key={podcast.slug}>
-          <a className={styles.podcast__link} href={`/podcast/${podcast.slug}`}>
+        <li className={styles.podcasts__podcast} key={podcast.slug}>
+          <a
+            className={styles.podcasts__podcast__link}
+            href={`/podcast/${podcast.slug}`}
+          >
             <Image
               image={podcast.image}
               sizes="100vw"
-              className={podcast.podcast__link__image}
+              className={styles.podcasts__podcast__link__image}
             />
 
-            <h2 className={styles.podcast__link__image__title}>
-              {podcast.title}
+            <h2 className={styles.podcasts__podcast__link__title}>
+              {podcast.episode}. {podcast.title}
             </h2>
 
-            <p className={styles.podcast__link__image__description}>
+            <p className={styles.podcasts__podcast__link__description}>
               {podcast.description}
             </p>
           </a>

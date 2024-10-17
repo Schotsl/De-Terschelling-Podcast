aa39ce0d042ce1c9ca052dea10a07203b3083eed
Sjors van Holst <sjorsvanholst@gmail.com>
2024-10-17 09:47:14 +0200

    🔍️ Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,17 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      allow: "/",
-      userAgent: "*",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/*.woff2$",
+          "/site.webmanifest",
+          "/proxy/api/event",
+        ],
+      },
+    ],
     sitemap: `${process.env.NEXT_PUBLIC_URL}/sitemap.xml`,
   };
 }
